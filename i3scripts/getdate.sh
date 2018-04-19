bd6e29a04d3dff3d503e335ba836479dbe5f9d37
luis-Desktop <luisfcdarcadia@gmail.com>
2018-04-19 03:37:16 -0300

    date script

--- i3scripts/getdate.sh
+++ i3scripts/getdate.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-daytemp="$(date +"%d")"
+# daytemp="$(date +"%d")"
 
-if (($daytemp == 11|12|13));then
-	day=$daytemp
-else
-	day=$(echo "${daytemp: -1}")
-fi
-
-if (( day == 1 )); then
-		ordinal=st
-elif (( day == 2 )); then
-		ordinal=nd
-elif (( day == 3 )); then
-		ordinal=rd
-else
-		ordinal=th
-fi
+# if (($daytemp == 11|12|13));then
+	# day=$daytemp
+# else
+	# day=$(echo "${daytemp: -1}")
+# fi
+# 
+# if (( day == 1 )); then
+		# ordinal=st
+# elif (( day == 2 )); then
+		# ordinal=nd
+# elif (( day == 3 )); then
+		# ordinal=rd
+# else
+		# ordinal=th
+# fi
 
 echo "$(date +'%A, %B %d')$ordinal - $(date +'%H:%M')"
